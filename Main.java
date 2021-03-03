4fbffe53c0dd8f914231092fb4f0864e4df00768
Vinicius Bergamasco <vinicius.bergamasco@hotmail.com>
2021-03-03 14:02:18 +0000

    Atividade concluida

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Olá, eu sou Vinicius e moro em Bebedouro.");
+    System.out.println("Olá, eu sou Vinicius Mingatos Bergamasco e moro em Bebedouro.");
   }
 }
